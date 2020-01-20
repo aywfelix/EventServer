5e5d8e048f894485f091b7e4c17c0a012393d5c4
aa <lai@126.com>
2020-01-20 23:33:11 +0800

    fix

--- Common/Assertx.h
+++ Common/Assertx.h
@@ -2,6 +2,9 @@
 
 #include "SePlatForm.h"
 #include <cassert>
+#ifdef SF_PLATFORM_WIN
+#include <crtdbg.h>
+#endif
 
 void __assert__(const char* file, uint32_t line, const char* func, const char* expr);
 void __assertex__(const char* file, uint32_t line, const char* func, const char* expr, const char* msg);
@@ -9,12 +12,6 @@ void __assertex__(const char* file, uint32_t line, const char* func, const char*
 #ifdef NDEBUG
 #define Assert(expr) ((void)0)
 #define AssertEx(expr, msg) ((void)0)
-#endif
-
-#ifdef SF_PLATFORM_WIN
-#include <crtdbg.h>
-#define Assert(expr) {if(!(expr)){ __assert__(__FILE__, __LINE__, __FUNCTION__, #expr);}}
-#define AssertEx(expr, msg) {if(!(expr)){ __assertex__(__FILE__, __LINE__, __FUNCTION__, #expr, msg);}}
 #else
 #define Assert(expr) {if(!(expr)){ __assert__(__FILE__, __LINE__, __FUNCTION__, #expr);}}
 #define AssertEx(expr, msg) {if(!(expr)){ __assertex__(__FILE__, __LINE__, __FUNCTION__, #expr, msg);}}
