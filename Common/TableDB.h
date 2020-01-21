f24939534577daac480a1337549e594a814ea44a
laijia <laijia2008@126.com>
2020-01-21 18:30:54 +0800

    fix

--- Common/TableDB.h
+++ Common/TableDB.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <string>
+
 class Table
 {
 public:
@@ -8,6 +10,29 @@ private:
 // 对应数据库表
 struct AccountTB
 {
-
+	int id;
+	std::string loginname;
+	std::string loginpwd;
+	std::string gameid;
+	std::string openid;
+	std::string token;
+	std::string channelid;
+	std::string whitelist;
+	std::string snid;
+	std::string mac;
 };
 
+struct PlayerTB
+{
+	std::string playerid;
+	std::string gameid;
+	int roleid;
+	std::string nickname;
+	int sex;
+	int level;
+	std::string create_tm;
+	std::string online_tm;
+	std::string offline_tm;
+	int gold;
+	std::string guildid;
+};
